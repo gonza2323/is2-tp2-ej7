dee04c8cd48ecbbe14bc4f60200aa062b7ff5e48
Gonzalo Padilla <107971962+gonza2323@users.noreply.github.com>
2025-10-30 12:03:07 -0300

    fix

--- api-rest/frontend/src/api/resources/account.ts
+++ api-rest/frontend/src/api/resources/account.ts
@@ -3,6 +3,5 @@ import { UsuarioDetailDto } from '../dtos';
 
 export async function getAccountInfo() {
   const response = await client.get('auth/me');
-  console.log(response);
   return UsuarioDetailDto.parse(response.data);
 }
